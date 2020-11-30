87fd918d96fd01a7b8f9363f88196cbcc13c8ea8
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-30 11:11:28 +0530

    TX/RX code and model

---
+++ wless_robo_rx/wless_robo_rx.ino
@@ -0,0 +1,72 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(13,11,5,4,3,2);
+int R2=10,R1=9,L2=8,L1=7;
+
+void setup()
+{ 
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  pinMode(L1,OUTPUT);
+  pinMode(L2,OUTPUT);
+  pinMode(R1,OUTPUT);
+  pinMode(R2,OUTPUT);
+}
+
+void loop()
+{
+  
+  Serial.print("work log : ");
+  int action=Serial.read();
+  
+  if(action==20){
+    Serial.println("moving forward");
+    digitalWrite(R1,0);
+    digitalWrite(R2,1);
+    digitalWrite(L1,0);
+    digitalWrite(L2,1);
+    lcd.clear();
+    lcd.print("MOVE-FORWARD");
+  }
+  else if(action==30){
+    Serial.println("moving right");
+    digitalWrite(R1,1);
+    digitalWrite(R2,0);
+    digitalWrite(L1,0);
+    digitalWrite(L2,1);
+    lcd.clear();
+    lcd.print("TURN-RIGHT"); 
+  }
+  else if(action==40)
+  {
+    Serial.println("moving reverse");
+    digitalWrite(R1,1);
+    digitalWrite(R2,0);
+    digitalWrite(L1,1);
+    digitalWrite(L2,0);
+    lcd.clear();
+    lcd.print("REVERSE");
+  }
+  else if(action==50){
+    Serial.println("moving left");
+    digitalWrite(R1,0);
+    digitalWrite(R2,1);
+    digitalWrite(L1,1);
+    digitalWrite(L2,0);
+    lcd.clear();
+    lcd.print("TURN-LEFT");
+  }
+  else// if(action==-1)
+  {
+    Serial.println("no command : no movement");
+    digitalWrite(R1,0);
+    digitalWrite(R2,0);
+    digitalWrite(L1,0);
+    digitalWrite(L2,0);
+    lcd.clear();
+    lcd.print("HALTED");
+  }
+  Serial.println();
+  delay(250);  
+}
---
+++ wless_robo_tx/wless_robo_tx.ino
@@ -0,0 +1,53 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(13,11,9,8,7,6);
+
+int FORWARD = 2;
+int RIGHT = 3;
+int REVERSE = 4;
+int LEFT = 5;
+
+void setup()
+{
+  lcd.begin(20,4);
+  Serial.begin(9600);
+  pinMode(FORWARD,INPUT_PULLUP);
+  pinMode(RIGHT,INPUT_PULLUP);
+  pinMode(REVERSE,INPUT_PULLUP);
+  pinMode(LEFT,INPUT_PULLUP);
+  lcd.setCursor(2,0);
+  lcd.print("FORWARD ^");
+  lcd.setCursor(4,1);
+  lcd.print("LEFT <");
+  lcd.setCursor(11,1);
+  lcd.print("> RIGHT");
+  lcd.setCursor(2,2);
+  lcd.print("REVERSE v");
+}
+
+void loop()
+{
+  int val;
+  if(digitalRead(FORWARD)==LOW)
+  {
+    val=20;
+    Serial.write(val);
+  }
+  else if(digitalRead(RIGHT)==LOW)
+  {
+    val=30;
+    Serial.write(val);
+  }
+  else if(digitalRead(REVERSE)==LOW)
+  {
+    val=40;
+    Serial.write(val);
+  }
+  else if(digitalRead(LEFT)==LOW)
+  {
+    val=50;
+    Serial.write(val);
+  }
+    //  Serial.write(val);
+  delay(250);
+}
